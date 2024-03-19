667c849ed32325e73863f5d9aa7594cd701f9e0d
Agustin <agustin.ledezma@ucb.edu.bo>
2024-03-19 00:03:28 -0400

    Se anadio un test para controlar un strike y tambien codigo en la fun… …cion

--- src/bowlingKata.js
+++ src/bowlingKata.js
@@ -1,32 +1,39 @@
 class Game {
-    constructor() {
-        this._rolls = new Array(21);
-        this._currentRoll = 0;
-    }
-    roll(pins){
-        this._rolls[this._currentRoll++]=pins;
-    }
-    score(){
-        let score=0
-        let i=0
-        for (let frame=0; frame< 10;frame++)
-        {
-            if (this.isSpare(i))//spare
-            {
-              score=score+10+this._rolls[i+2]
-              i=i+2
-            }
-            else
-            {
-              score=score+this._rolls[i]+this._rolls[i+1]
-              i=i+2
-            }
-        }
-        return score
-    }
-
-    isSpare(i){
-        return this._rolls[i]+this._rolls[i+1]==10
-      }
+  constructor() {
+    this._rolls = new Array(21);
+    this._currentRoll = 0;
   }
-  export default Game;
+  roll(pins){
+    this._rolls[this._currentRoll++]=pins;
+ 
+  }
+  score(){
+    let score=0
+    let i=0
+    for (let frame=0; frame< 10;frame++)
+    {
+      if (this._rolls[i]==10) //strike
+      {
+        score=score+10+this._rolls[i+1]+this._rolls[i+2]
+        i++
+      }
+      else if (this.isSpare(i))//spare
+      {
+        score=score+10+this._rolls[i+2]
+        i=i+2
+      }
+      else
+      {
+        score=score+this._rolls[i]+this._rolls[i+1]
+        i=i+2
+      }
+      
+    }
+    return score
+  }
+  isSpare(i){
+    return this._rolls[i]+this._rolls[i+1]==10
+  }
+  
+}
+export default Game;
--- src/bowlingKata.spec.js
+++ src/bowlingKata.spec.js
@@ -41,10 +41,18 @@ describe("bowlingKata", () => {
   rollMany(17,0)
   expect(g.score()).toEqual(16);
   });
-  
+
   function rollSpare(){
     g.roll(5)
     g.roll(5)
   }
+
+  it("Probando un strike", () => {
+    g.roll(10)
+    g.roll(3)
+    g.roll(4)
+    rollMany(16,0)
+    expect(g.score()).toEqual(24);
+  });
 
 });
